f1bbc3fe66c965634383513f424f48fe6afda7e4
Brian Blank <beblank@gmail.com>
2024-04-23 22:16:54 -0500

    adds app.py and response.json

---
+++ app.py
@@ -0,0 +1,32 @@
+
+from flask import Flask,jsonify,request 
+import pandas as pd
+import json
+
+
+with open("response.json", "r") as file:
+    json_data = file.read()
+data = json.loads(json_data)
+results = data['result']
+r = pd.DataFrame(results)
+
+
+def ReturnJsonResultOuterKey(df, user):
+    df = df[df['user'] == user]
+    outer_key = "result"
+    data = df.to_json(orient='records',double_precision=15)
+    data = json.loads(data)
+    data_with_outer_key = {outer_key: data}
+    json_with_outer_key = json.dumps(data_with_outer_key, indent=4)
+    return json_with_outer_key
+
+
+app =   Flask(__name__) 
+  
+@app.route('/returnjson', methods = ['GET']) 
+def ReturnJSON(): 
+    user = request.args.get('user')
+    if(request.method == 'GET'): 
+        return ReturnJsonResultOuterKey(r, user)
+
+    
